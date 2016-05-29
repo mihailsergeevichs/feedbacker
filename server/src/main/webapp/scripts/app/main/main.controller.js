983224e8e55e571e39b7646a438154948659ee66
Mihail Sergeevich <mihailsergeevichs@gmail.com>
2016-05-29 17:06:39 +0300

    fixed main page table, app working

--- server/src/main/webapp/scripts/app/main/main.controller.js
+++ server/src/main/webapp/scripts/app/main/main.controller.js
@@ -1,9 +1,47 @@
 'use strict';
 
 angular.module('feedbackerApp')
-    .controller('MainController', function ($scope, Principal) {
+    .controller('MainController', function ($scope, $state, Principal, Feedback, ParseLinks) {
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
         });
+        $scope.feedbacks = [];
+        $scope.predicate = 'id';
+        $scope.reverse = true;
+        $scope.page = 0;
+        $scope.loadAll = function() {
+            Feedback.query({page: $scope.page, size: 20, sort: [$scope.predicate + ',' + ($scope.reverse ? 'asc' : 'desc'), 'id']}, function(result, headers) {
+                $scope.links = ParseLinks.parse(headers('link'));
+                for (var i = 0; i < result.length; i++) {
+                    $scope.feedbacks.push(result[i]);
+                }
+            });
+        };
+        $scope.reset = function() {
+            $scope.page = 0;
+            $scope.feedbacks = [];
+            $scope.loadAll();
+        };
+        $scope.loadPage = function(page) {
+            $scope.page = page;
+            $scope.loadAll();
+        };
+        $scope.loadAll();
+
+        $scope.refresh = function () {
+            $scope.reset();
+            $scope.clear();
+        };
+
+        $scope.clear = function () {
+            $scope.feedback = {
+                date: null,
+                quality: null,
+                speed: null,
+                price: null,
+                commentary: null,
+                id: null
+            };
+        };
     });
